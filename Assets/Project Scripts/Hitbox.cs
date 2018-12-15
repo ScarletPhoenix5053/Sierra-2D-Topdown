83078ca86204cb358a8a8ce3ae969a863f719f1d
ScarletPhoenix5053 <45509046+ScarletPhoenix5053@users.noreply.github.com>
2018-12-15 16:15:04 +1300

    Updated hitbox

--- Assets/Project Scripts/Hitbox.cs
+++ Assets/Project Scripts/Hitbox.cs
@@ -3,35 +3,28 @@ using System.Collections.Generic;
 
 namespace Sierra.Unity2D.TopDown
 {
+    /// <summary>
+    /// When active, checks for collisions with colliders attached to a Health component and sends a damage command to it if it finds one.
+    /// </summary>
     public class Hitbox : MonoBehaviour
     {
         public LayerMask LayerMask;
-        private Collider _collider;
-        private List<Health> _checked;
-        private AttackData _data;
 
-        // !!! MAKE ENABLING/DISABLING COLLIDER A METHOD IN THIS CLASS AND USE IT TO PASS A REFRENCE TO ATTACKDATA
-        private void Awake()
+        protected Collider _collider;
+        protected List<Health> _checked;
+        protected AttackData _data;
+        
+        protected void Awake()
         {
             _collider = GetComponent<Collider>();
         }
-        private void OnTriggerStay(Collider other)
+        protected void Update()
         {
-            Health health;
-            if (health = other.GetComponent<Health>())
-            {
-                if (_checked == null) _checked = new List<Health>();
-                foreach (Health h in _checked)
-                {
-                    if (health = h) return;
-                }
-                health.RemoveHp(_data);
-                _checked.Add(health);
-            }
+            ResetColliderWhenInactive();
         }
-        private void Update()
+        protected void OnTriggerStay(Collider other)
         {
-            if (!_collider.enabled && _checked != null) _checked = null;
+            CheckForTarget(other);
         }
 
         public void ActivateHitBox(AttackData data)
@@ -43,6 +36,30 @@ namespace Sierra.Unity2D.TopDown
         {
             _collider.enabled = false;
             _data = null;
+        }
+
+        protected void CheckForTarget(Collider other)
+        {
+            Health health;
+
+            // Check for health component
+            if (health = other.GetComponent<Health>())
+            {
+                // Check if health component has already been sent a command
+                if (_checked == null) _checked = new List<Health>();
+                foreach (Health h in _checked)
+                {
+                    if (health = h) return;
+                }
+
+                // If not, send command and add to checked list
+                health.RemoveHp(_data);
+                _checked.Add(health);
+            }
+        }
+        protected void ResetColliderWhenInactive()
+        {
+            if (!_collider.enabled && _checked != null) _checked = null;
         }
     }
 }
